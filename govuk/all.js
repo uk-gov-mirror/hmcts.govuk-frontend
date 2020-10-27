77ba25bfbef72c0e335f7304a70de617b2969ee9
Hanna Laakso <hanna.laakso@digital.cabinet-office.gov.uk>
2020-10-27 18:08:55 +0000

    Release GOV.UK Frontend to 'pre-release-add-notifications-auto-focus' for testing

--- govuk/all.js
+++ govuk/all.js
@@ -1959,6 +1959,54 @@ ErrorSummary.prototype.getAssociatedLegendOrLabel = function ($input) {
     $input.closest('label')
 };
 
+function NotificationBanner ($module) {
+  this.$module = $module;
+}
+
+// Initialise component
+NotificationBanner.prototype.init = function () {
+  var $module = this.$module;
+  // Check for module
+  if (!$module) {
+    return
+  }
+
+  // Focus the element when the HTML document has loaded
+  window.addEventListener('DOMContentLoaded', this.setFocus.bind(this));
+};
+
+/**
+ * Focus the element
+ *
+ * If `role="alert"` is set, focus the element to help some assistive technologies
+ * prioritise announcing it.
+ *
+ * This function should be called *after* the `DOMContentLoaded` event is fired.
+ *
+ * You can turn off the auto-focus functionality by setting `data-disable-auto-focus="true"` in the * component HTML. You might wish to do this based on user research findings, or to avoid
+ * a clash with another element which should be focused when the page loads.
+ */
+NotificationBanner.prototype.setFocus = function () {
+  var $module = this.$module;
+
+  // Check if the attribute that disables the auto focus is present
+  if ($module.getAttribute('data-disable-auto-focus') === 'true') {
+    return
+  }
+
+  // Check if `role="alert"` is set. When this role is added to an element, the browser will send out an accessible alert event to assistive technology products which can then notify the user about it.
+  if ($module.getAttribute('role') !== 'alert') {
+    return
+  }
+
+  // Set tabindex to -1 so to make the element focusable with JavaScript (the element will not be reachable via sequential keyboard navigation).
+  if (!$module.getAttribute('tabindex')) {
+    $module.setAttribute('tabindex', '-1');
+  }
+
+  this.$module.focus();
+};
+
 function Header ($module) {
   this.$module = $module;
   this.$menuButton = $module && $module.querySelector('.govuk-js-header-toggle');
@@ -2475,6 +2523,11 @@ function initAll (options) {
   // Find first header module to enhance.
   var $toggleButton = scope.querySelector('[data-module="govuk-header"]');
   new Header($toggleButton).init();
+
+  var $notificationBanners = scope.querySelectorAll('[data-module="govuk-notification-banner"]');
+  nodeListForEach($notificationBanners, function ($notificationBanner) {
+    new NotificationBanner($notificationBanner).init();
+  });
 
   var $radios = scope.querySelectorAll('[data-module="govuk-radios"]');
   nodeListForEach($radios, function ($radio) {
